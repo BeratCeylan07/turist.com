969fd928943b028843670fc0eec2b6d7dc5717c6
BeratCeylan07 <124052396+BeratCeylan07@users.noreply.github.com>
2024-06-21 16:27:01 +0300

    turist.com first commit
    turist.com first commit

---
+++ onlineucakbilet/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var webViewStore = WebViewStore()
+    
+    var body: some View {
+        ZStack {
+            Color.init("Color").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            WebView(webView: webViewStore.webView)
+            
+        }.onAppear {
+            self.webViewStore.webView.load(URLRequest(url: URL(string: "https://turist.com")!))
+        }
+    }
+}
